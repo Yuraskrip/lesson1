7388ea6e707912129009bc7ca75e03c63a0f106c
Yuraskrip <kozakskrip@gmail.com>
2022-04-27 20:51:27 +0300

    Добавьте файлы проекта.

---
+++ Lesson1/lesson1/lesson1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваше имя");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}!, сегодня {DateTime.Now}");
+
+
+            Console.ReadLine(); 
+            //не понял где именно нужно поставить точку останова, но тут программа работает
+        }
+
+
+    }
+
+}
+
